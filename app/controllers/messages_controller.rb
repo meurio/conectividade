601e3d3c34046430a247e7b9510871f73845919f
Josias Schneider <josias.ds@gmail.com>
2015-07-23 11:26:46 -0300

    [#99453424] test messages controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,11 +8,14 @@ class MessagesController < ApplicationController
   def create
     user_params = message_params[:user_attributes]
     user = User.create_with(user_params).find_or_create_by(email: user_params[:email])
+
+    Message.create(user: user)
+
     redirect_to root_path
   end
 
   private
   def message_params
-    params.require(:message).permit(user_attributes: [:first_name, :last_name, :email])
+    params.fetch(:message, {}).permit(user_attributes: [:first_name, :last_name, :email])
   end
 end
--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -19,4 +19,27 @@ RSpec.describe MessagesController, type: :controller do
       expect(assigns :users_count).to eq(2)
     end
   end
+
+  describe 'POST #create' do
+    context 'when the data is invalid' do
+      it 'does not create a new Message' do
+        expect { post :create }.to raise_error { ActionController::ParameterMissing }
+      end
+    end
+
+    context 'when the params are valid' do
+      it 'creates a new message' do
+        valid_user = attributes_for(:user)
+        expect { post :create,
+          message: {
+            user_attributes: {
+              first_name: valid_user[:first_name],
+              last_name: valid_user[:last_name],
+              email: valid_user[:email]
+            }
+          }
+        }.to change { Message.count }.by(1)
+      end
+    end
+  end
 end
